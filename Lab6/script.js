bd10e8a37bd3a4379379ed55769676d3c8e6225e
Kostia Semkiv <semksvk@gmail.com>
2024-04-17 06:00:52 +0300

    finish

--- Lab6/script.js
+++ Lab6/script.js
@@ -1,12 +1,41 @@
-const GRID_SIZE = 5; // Розмір сітки
+let data = null;
+const gridSize = 5;
+let grid = null;
+let minMoves = null;
+let indexGrid=0;
+let moves=0;
+const victoryMessage = document.getElementById("victory-message");
 
-// Створення інтерфейсу гри
+function updateText(id, text) {
+    document.getElementById(id).textContent = text;
+}
+function fetchData() {
+    fetch(
+        "https://raw.githubusercontent.com/brehniv/JSUniversity/main/Lab6/json/response.json"
+    )
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("[P1]: Fetch Error");
+            }
+            return response.json();
+        })
+        .then((jsonData) => {
+            data = jsonData;
+            initLights();
+        })
+        .catch((error) => {
+            console.error("Fetching error:", error);
+        });
+}
+function deepClone(data) {
+    return JSON.parse(JSON.stringify(data));
+}
 function createGameBoard() {
     const gameBoard = document.getElementById("game-board");
-    for (let i = 0; i < GRID_SIZE; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        for (let j = 0; j < GRID_SIZE; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell");
             cell.dataset.row = i;
@@ -17,7 +46,20 @@ function createGameBoard() {
     }
 }
 
-// Оновлення стану сітки
+function initLights() {
+    if (!data) return;
+    grid = deepClone(data[indexGrid].grid);
+    minMoves = data[indexGrid].min_to_solve;
+    updateText("min-moves",minMoves);
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell, index) => {
+        const row = Math.floor(index / gridSize);
+        const col = index % gridSize;
+        const state = grid[row][col] ? "on" : "off";
+        cell.classList.toggle("on", state === "on");
+        cell.classList.toggle("off", state === "off");
+    });
+}
 function updateGrid() {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
@@ -28,38 +70,71 @@ function updateGrid() {
         cell.classList.toggle("off", state === "off");
     });
 }
-
-// Зміна стану вимикача та його сусідів
-function toggleSwitch(row, col) {
+const toggleSwitch = (row, col) => {
     grid[row][col] = !grid[row][col];
     if (row > 0) grid[row - 1][col] = !grid[row - 1][col];
-    if (row < GRID_SIZE - 1) grid[row + 1][col] = !grid[row + 1][col];
+    if (row < gridSize - 1) grid[row + 1][col] = !grid[row + 1][col];
     if (col > 0) grid[row][col - 1] = !grid[row][col - 1];
-    if (col < GRID_SIZE - 1) grid[row][col + 1] = !grid[row][col + 1];
-}
+    if (col < gridSize - 1) grid[row][col + 1] = !grid[row][col + 1];
+};
 
 // Обробка кліків по вимикачам
 function handleClick(event) {
+    moves++;
+    updateText("moves", moves);
+
     const cell = event.target;
     const row = parseInt(cell.dataset.row);
     const col = parseInt(cell.dataset.col);
     toggleSwitch(row, col);
     updateGrid();
+    if (winCheck()) finishGame();
+}
+function finishGame() {
+    victoryMessage.style.display = "block";
+    const message = document.getElementById("optimal-moves-count");
+    if (moves <= minMoves) {
+        message.textContent = "You solved it in an optimal number of moves!";
+    } else {
+        message.textContent = "You solved it, but it took more moves than optimal.";
+    }
+}
+function newGame() {
+    indexGrid++;
+    if(indexGrid==3)indexGrid = 0;
+    fetchData();
+    moves=0;
+    updateText("moves", moves);
+    victoryMessage.style.display = "none";
+}
+function restartGame() {
+    grid = deepClone(data[indexGrid].grid);
+    moves=0;
+    updateText("moves", moves);
+    updateText("min-moves", minMoves);
+    initLights();
+    victoryMessage.style.display = "none";
+}
+function winCheck() {
+    const cells = document.querySelectorAll(".cell");
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].classList.contains("off")) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Ініціалізація гри
 function initGame() {
-    toggleSwitch(2,3);
     createGameBoard();
-    updateGrid();
+    fetchData();
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
         cell.addEventListener("click", handleClick);
     });
+    
+    document.getElementById("new-game-btn").addEventListener("click", newGame);
+    document.getElementById("restart-btn").addEventListener("click", restartGame);
 }
 
-// Ініціалізація та початок гри
-const grid = Array.from({ length: GRID_SIZE }, () =>
-    Array(GRID_SIZE).fill(false)
-);
 initGame();
